2a122694e69bf930f7fe8d79c220fc5e249ad845
Wei Ning <nwcgnw@gmail.com>
2018-07-15 17:23:54 +1200

    - move variadic to /parameters dir
    - add non-type parameter example

---
+++ parameters/nonTypeParam.cpp
@@ -0,0 +1,29 @@
+
+/*
+source
+https://stackoverflow.com/questions/499106/what-does-template-unsigned-int-n-mean
+
+You can have several kinds of template parameters
+
+Type Parameters.
+Types
+Templates (only classes and alias templates, no functions or variable templates)
+
+Non-type Parameters
+Pointers
+References
+Integral constant expressions
+
+ */
+
+template<unsigned int S>
+struct Vector {
+    unsigned char bytes[S];
+};
+
+int main() {
+    // pass 3 as argument.
+    Vector<3> test{1, 1, 1};
+    static_assert(3 == sizeof(test), "");
+    return 0;
+}
