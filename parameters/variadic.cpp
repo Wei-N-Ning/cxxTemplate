d61836283527cea9cd66fb29a6eb56d4ae627c84
Wei Ning <wning@wetafx.co.nz>
2018-08-06 10:53:32 +1200

    - update variadic template parameter example

--- parameters/variadic.cpp
+++ parameters/variadic.cpp
@@ -4,6 +4,7 @@
 
 #include <cassert>
 #include <tuple>
+#include <string>
 
 template<typename... Ts>
 void foo(Ts&&... params) {
@@ -12,7 +13,23 @@ void foo(Ts&&... params) {
     assert((4 == sz));
 }
 
+int bar_impl(const std::string& i_s) {
+    return 1;
+}
+
+int bar_impl(int i, double d) {
+    return 2;
+}
+
+template<typename... Ts>
+int bar(Ts... params) {
+    return bar_impl(params...);
+}
+
 int main() {
     foo(1, 2.3, "asd", true);
+    std::string name("13");
+    assert(1 == bar(name));
+    assert(2 == bar(1, 1.0));
     return 0;
 }
