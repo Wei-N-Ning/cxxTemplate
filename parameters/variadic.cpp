95ee5c78bfd97b6fc65e896779e9eecfac63832e
Wei Ning <wning@wetafx.co.nz>
2018-08-07 07:31:12 +1200

    - update perfect forwarding example

--- parameters/variadic.cpp
+++ parameters/variadic.cpp
@@ -4,15 +4,12 @@
 
 // used this in wkou code base
 // recall EMC++ variadic parameter and perfect forwarding
-// TODO: 
-// 1. verify that for bar_impl(const std::string&), the callee indeed
-// gets a reference (not reconstructed)
-// 2. verify that Ts... params can work with any reference types
-// 3. forward() is indeed perfect (when using Ts&&...)
 
 #include <cassert>
 #include <tuple>
 #include <string>
+#include <cstring>
+#include <cstdio>
 
 template<typename... Ts>
 void foo(Ts&&... params) {
@@ -29,9 +26,28 @@ int bar_impl(int i, double d) {
     return 2;
 }
 
+// verify that the output pointer stays an output pointer
+int bar_impl(const std::string& i_s, std::string* o_str) {
+    *o_str = i_s;
+    return 3;
+}
+
+// verify that the output reference stays an output reference
+int bar_impl(const std::string& i_s, std::string& o_str) {
+    o_str = i_s;
+    return 4;
+}
+
+// EMC++ P167
+// "perfect forwarding (constructor)" P180
+// std::forward<decltype(params)>(params)...
+// without effective/perfect forwarding - i.e. if the wrap takes
+// arguments by value, bar(Ts... params) - it creates temporary objects 
+// from the references (const or not) then passes the temporary objects 
+// to the impl functions
 template<typename... Ts>
-int bar(Ts... params) {
-    return bar_impl(params...);
+int bar(Ts&&... params) {
+    return bar_impl(std::forward<decltype(params)>(params)...);
 }
 
 int main() {
@@ -39,5 +55,15 @@ int main() {
     std::string name("13");
     assert(1 == bar(name));
     assert(2 == bar(1, 1.0));
+    
+    // output parameter is a pointer
+    std::string receiver;
+    assert(3 == bar(name, &receiver));
+    assert(0 == strcmp(name.c_str(), receiver.c_str()));
+    
+    // output parameter is a mutable reference
+    std::string answer;
+    assert(4 == bar(name, answer));
+    assert(0 == strcmp(name.c_str(), answer.c_str()));
     return 0;
 }
