ee92d7e9b7b06833ad56340c6f53995b7bda7d0c
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-04-25 14:59:59 +1000

    working on class template

---
+++ class/stack.cpp
@@ -0,0 +1,52 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "doctest/doctest.h"
+
+#include <vector>
+#include <optional>
+#include <initializer_list>
+
+// std::optional
+// read: https://en.cppreference.com/w/cpp/utility/optional
+// None is expressed as std::optional{}
+
+template<typename T>
+class Stack {
+public:
+    Stack() = default;
+    Stack(std::initializer_list<T> il) : vec(il) {}
+
+    void push(const T &elem) { vec.push_back(elem); }
+
+    std::optional<T> pop() {
+        if (vec.empty()) {
+            return std::optional<T>{};
+        }
+        std::optional<T> v{vec.back()};
+        vec.pop_back();
+        return v;
+    }
+
+    std::optional<T> top() {
+        return vec.empty() ? std::optional<T>{} : std::optional<T>{vec.back()};
+    }
+
+    auto empty() { return vec.empty(); }
+
+private:
+    std::vector<T> vec;
+};
+
+TEST_CASE ("push pop") {
+    Stack<int> s{1, 2};
+    CHECK_FALSE(s.empty());
+    CHECK_EQ(s.pop(), 2);
+    CHECK_EQ(s.top(), 1);
+    CHECK_EQ(s.pop(), 1);
+    CHECK(s.empty());
+}
+
+TEST_CASE ("effect") {
+    Stack<int> s;
+    CHECK_FALSE(s.pop().has_value());
+}
