5879b13f62048511f2046ec292c93d12261fe1dd
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-05-07 16:23:16 +1000

    notes

---
+++ conditional/is_fundamental.cpp
@@ -0,0 +1,12 @@
+
+// source:
+// mastering c++ programming L1285
+//
+TEST_CASE ("") {
+    std::is_fundamental_v<int>;
+    std::is_compound_v<Point>;
+
+    // fundamental: int, double, char, void
+    //  compound: ptr, array, class
+}
+
