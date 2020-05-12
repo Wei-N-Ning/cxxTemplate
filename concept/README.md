4178ac2f34445aa0652347a29bbe4f2c13182232
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-05-12 13:49:17 +1000

    init c++20 concept

---
+++ concept/README.md
@@ -0,0 +1,3 @@
+# C++20 Concept
+
+Move it to cxxFP as it has a closer relation with FP's type constraint system.
---
+++ concept/constraint.cpp
@@ -0,0 +1,22 @@
+//
+// Created by weining on 12/5/20.
+//
+
+// can not include <concept> header
+
+#include <iostream>
+
+template<typename T>
+concept addable = requires (T obj) { obj + obj; };
+
+template<addable T>
+T sum(T a, T b) {
+    return a + b;
+}
+
+int main() {
+    using namespace std;
+    cout << sum(1, 2) << endl;
+    cout << sum('a', 'b') << endl;
+    return 0;
+}
