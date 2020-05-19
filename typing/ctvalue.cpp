7ec411c5b21e1d46d3ce0fee28d53f8274e0c6c8
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-05-20 08:04:03 +1000

    ctvalue

---
+++ typing/ctvalue.cpp
@@ -0,0 +1,15 @@
+//
+// Created by weining on 20/5/20.
+//
+
+// c++ template: complete guide L19535
+// compile-time value (CTValue)
+// this follows the research done on typelist and related algorithms.
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "doctest/doctest.h"
+
+TEST_CASE ("") {
+    ;
+}
