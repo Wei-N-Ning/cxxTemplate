56fdcb611e48243b0200c87490096a6f8e6ae92b
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-05-19 15:53:35 +1000

    more advanced algorithms for TypeList

--- typing/typelist.cpp
+++ typing/typelist.cpp
@@ -5,11 +5,17 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
 #include "doctest/doctest.h"
+#include <tuple>
 
 // c++ template: complete guide L18944
 
 template<typename... Elements>
+// define my own TypeList
 class TypeList {};
+// or use std::tuple as the TypeList
+// most algorithms below work for both definitions
+// !!!HOWEVER the LargestTypeT recursion will not!!!
+//using TypeList = std::tuple<Elements...>;
 
 using SignedIntTypes = TypeList<signed char, short, int, long long>;
 
@@ -49,8 +55,56 @@ public:
 template<typename List, typename NewElement>
 using PushFront = typename PushFrontT<List, NewElement>::Type;
 
-TEST_CASE ("") {
+// c++ templates: complete guide L19006
+// type list algorithms
+// indexing
+template<typename List, unsigned N>
+struct NthElementT : public NthElementT<PopFront<List>, N - 1> {};
+
+// base case
+template<typename List>
+struct NthElementT<List, 0> : public FrontT<List> {};
+
+template<typename List, unsigned N>
+using NthElement = typename NthElementT<List, N>::Type;
+
+// c++ template: complete guide L19063
+// finding the largest type within the typelist
+template<typename List>
+struct LargestTypeT;
+
+// recursive case
+template<typename List>
+struct LargestTypeT {
+    using First = Front<List>;
+    using Rest = typename LargestTypeT<PopFront<List>>::Type;
+    using Type =
+        std::conditional_t<(sizeof(First) > sizeof(Rest)), First, Rest>;
+};
+
+// base case
+template<>
+struct LargestTypeT<TypeList<>> {
+    using Type = char;
+};
+
+template<typename List>
+using LargestType = typename LargestTypeT<List>::Type;
+
+TEST_CASE ("push pop") {
     static_assert(std::is_same_v<Front<SignedIntTypes>, signed char>);
     static_assert(std::is_same_v<Front<PopFront<SignedIntTypes>>, short>);
     static_assert(std::is_same_v<Front<PushFront<SignedIntTypes, char>>, char>);
+}
+
+TEST_CASE ("indexing") {
+    static_assert(
+        std::is_same_v<long long, NthElement<SignedIntTypes, 3>>
+        );
+}
+
+TEST_CASE ("largest type") {
+    static_assert(
+        std::is_same_v<long long, LargestType<SignedIntTypes>>
+    );
 }
