a035c7b7106c282073d617a888f3178ac8753143
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-12-14 16:09:11 +1100

    review and clean up

--- compilation/attributes.cpp
+++ compilation/attributes.cpp
@@ -5,29 +5,35 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest/doctest.h"
 
-enum class [[nodiscard]] Important {
+enum class [[nodiscard]] Important{
     Yes,
     No,
 };
 
-struct [[nodiscard]] Answer {
+struct [[nodiscard]] Answer
+{
 };
 
-[[deprecated("Use func2()")]] void func() {
+[[deprecated( "Use func2()" )]] void
+func()
+{
 }
 
 // without [[maybe_unused]] this results in compilation error
-void func2([[maybe_unused]] int a) {
+void
+func2( [[maybe_unused]] int a )
+{
     ;
 }
 
-TEST_CASE ("") {
+TEST_CASE( "" )
+{
     // -Wall will turn this into a compilation error
     auto o = []() { return Important::Yes; }();
-    CHECK_EQ(o, Important::Yes);
+    CHECK_EQ( o, Important::Yes );
 
     auto answer = []() { return Answer{}; }();
-    CHECK(&answer != nullptr);
+    CHECK( &answer != nullptr );
 
     // result in compilation error
     // func();
--- deduction/deduce_from_func_arg.cpp
+++ deduction/deduce_from_func_arg.cpp
@@ -8,11 +8,14 @@
 // complete guide L7225
 // typename B is explicitly specified at the call site whereas A is deduced from
 // the function arguments
-template<typename B, typename A>
-B implicit_cast(const A &a) {
+template< typename B, typename A >
+B
+implicit_cast( const A& a )
+{
     return a;
 }
 
-TEST_CASE ("") {
-    CHECK_EQ(1, implicit_cast<int>(1.0));
+TEST_CASE( "" )
+{
+    CHECK_EQ( 1, implicit_cast< int >( 1.0 ) );
 }
