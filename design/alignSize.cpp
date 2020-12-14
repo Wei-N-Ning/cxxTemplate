ffef8acd516bf2e68efb066662704e33b83cebf4
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-12-14 16:05:04 +1100

    restructure

--- design/alignSize.cpp
+++ design/alignSize.cpp
@@ -43,3 +43,23 @@ TEST_CASE ("") {
     // the resulting value is available at compile time
     char ignore[unnamed.value];
 }
+
+class Foo {};
+
+class Bar : private Foo {};
+class FooBar : private Bar {};
+
+TEST_CASE ("") {
+    using namespace std;
+
+    // even empty classes, however, have non-zero size
+    // for many platforms this prints 1
+    // a few systems impose more strict alignment requirements on class types
+    // and may print another small integer
+    cout << sizeof(Foo{}) << endl;
+    // it prints the same size for every class, but none of these has
+    // size zero
+    // this means that within class Bar, the class Foo is not given any space
+    cout << sizeof(Bar{}) << endl;
+    cout << sizeof(FooBar{}) << endl;
+}
--- type_level_fun/ctvalue_helper.cpp
+++ type_level_fun/ctvalue_helper.cpp
@@ -8,17 +8,19 @@
 // complete guide L3503
 // a useful application of variable templates is to define variables that
 // represent members of class templates
-template<typename T>
-struct is_signed {
+template< typename T >
+struct is_signed
+{
     // constexpr must be a static member
-    static constexpr bool value = std::numeric_limits<T>::is_signed;
+    static constexpr bool value = std::numeric_limits< T >::is_signed;
 };
 
-TEST_CASE ("") {
+TEST_CASE( "" )
+{
     // since C++17. the standard library uses the technique of variable
     // templates to define shortcuts for all type traits in the standard
     // library that yield a bool value
     // ..._v
-    CHECK(is_signed<int>::value);
-    CHECK_FALSE(is_signed<bool>::value);
+    CHECK( is_signed< int >::value );
+    CHECK_FALSE( is_signed< bool >::value );
 }
