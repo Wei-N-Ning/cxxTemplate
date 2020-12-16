cc4190d4c9a3d3e4df594b0f17e99af59e82ee6c
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-12-16 20:58:49 +1100

    C++17: review and clean up

--- deduction/deduce_class_template.cpp
+++ deduction/deduce_class_template.cpp
@@ -6,6 +6,8 @@
 #include "doctest/doctest.h"
 
 #include <utility>
+#include <type_traits>
+#include <variant>
 
 // C++ 17 in detail P43
 
@@ -15,6 +17,57 @@ class TypeTester;
 TEST_CASE( "deduce class template argument automatically" )
 {
     // NOTE I can not write (1, "iddqd") as the latter will be deduced to const char *
+    // unless I provide my own deduction rule
     auto pr = std::make_pair( 1, std::string{ "i" } );
     // TypeTester< decltype( pr ) >();
+}
+
+template< typename T >
+struct Wrapper
+{
+    using wrapped_t = T;
+    T str;
+};
+
+// C++17 in detail P/45
+// the compiler uses special rules called deduction guides to work out the
+// template class types.
+// we have two types of rules: compiler generated (implicitly generated) and
+// user-defined.
+Wrapper( const char* )->Wrapper< std::string >;
+
+// this won't work
+// std::make_pair( const char*, const char* )->std::pair< std::string, std::string >;
+
+TEST_CASE( "custom deduction rule" )
+{
+    auto w = Wrapper{ "idkfa" };
+    static_assert( std::is_same_v< decltype( w )::wrapped_t, std::string > );
+}
+
+// type deduction rule can be used to model sum-type pattern matching using
+// std::variant; see cxxFP/types/sum_type for another example
+template< typename... Ts >
+struct overloaded : Ts...
+{
+    using Ts::operator()...;
+};
+
+template< typename... Ts >
+overloaded( Ts... ) -> overloaded< Ts... >;
+
+using Product = std::variant< int, char, float, double >;
+using Result = int;
+
+TEST_CASE( "model sum-type pattern matching" )
+{
+    auto xs = { Product{ 1 }, Product{ 'a' }, Product{ 1.1f }, Product{ 1.11 } };
+    auto f = overloaded( []( const int& x ) -> Result { return 0; },
+                         []( const char& x ) -> Result { return 1; },
+                         []( const float& x ) -> Result { return 2; },
+                         []( const double& x ) -> Result { return 3; } );
+    for ( const auto& x : xs )
+    {
+        std::visit( f, x );
+    }
 }
--- type_level_fun/parampack/variadic_fold.cpp
+++ type_level_fun/parampack/variadic_fold.cpp
@@ -4,6 +4,8 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest/doctest.h"
 
+#include <iostream>
+
 // C++17 new syntax: simplified fold for parameter pack
 
 template< typename T >
@@ -19,9 +21,70 @@ foldThese( Ts... args )
     ( foldThis( args ), ... );
 }
 
+// C++17 in detail P/47
+// the following variations of fold expressions with binary op exist
+// (... op e)
+// unary left fold: ((e1 op e2) op ...) op eN
+// (init op ... op e)
+// binary left fold: init op e1) op e2) op ...) op eN
+// (e op ...)
+// unary right fold: e1 op (... op (eN-1 op eN
+// (e op ... op init)
+// binary right fold: e1 op (... op (eN-1 op (eN op init
+
 TEST_CASE( "c++17 new syntax for fold variadic template parameter pack" )
 {
     foldThese( 1, 2.0f, 2.1, 10l, 10u, 'c' );
+}
+
+template< typename Map, typename Filter, typename Acc, typename T >
+T
+process( Map map, Filter filter, Acc acc, T init, T arg )
+{
+    T x = map( arg );
+    if ( filter( x ) )
+    {
+        return acc( init, x );
+    }
+    else
+    {
+        return init;
+    }
+}
+
+template< typename Map, typename Filter, typename Acc, typename T, typename... Ts >
+T
+mapFilterAcc( Map map, Filter filter, Acc acc, T init, T head, Ts... tail )
+{
+    // check parameter pack length:
+    // https://stackoverflow.com/questions/2770474/how-to-find-the-length-of-a-parameter-pack
+    // also inspired by the example on this page:
+    // https://en.cppreference.com/w/cpp/language/parameter_pack
+    T x = map( head );
+    if constexpr ( sizeof...( tail ) == 0 )
+    {
+        return filter( x ) ? acc( init, x ) : init;
+    }
+    else
+    {
+        return filter( x ) ? acc( x, mapFilterAcc( map, filter, acc, init, tail... ) )
+                           : mapFilterAcc( map, filter, acc, init, tail... );
+    }
+}
+
+TEST_CASE( "map filter accumulate variadic recursion" )
+{
+    // NOTE: this is not using the variadic fold expression which only
+    // works with an operator (one of the 32 operators);
+    // If I want to provide my own pure functions, I need to use pattern
+    // match and recursion
+    auto map = []( const int& x ) { return x + 1; };
+    auto filter = []( const int& x ) { return x > 2; };
+    auto acc = []( const int& a, const int& b ) { return a + b; };
+    auto o = mapFilterAcc( map, filter, acc, 0, 1, 2, 3, 4 );
+    // the result of map()                               2  3  4  5
+    // the result of filter()                               ^  ^  ^
+    std::cout << o << '\n';
 }
 
 // complete guide L7766
