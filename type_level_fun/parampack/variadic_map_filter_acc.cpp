f6ee92ddf1a0af9432cdf817f67ef6566c2c9f26
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-12-16 21:27:58 +1100

    C++17: review and clean up

--- type_level_fun/parampack/variadic_map_filter_acc.cpp
+++ type_level_fun/parampack/variadic_map_filter_acc.cpp
@@ -5,23 +5,35 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest/doctest.h"
 
+#include <iostream>
+
 template< typename Map, typename Filter, typename Acc, typename T, typename... Ts >
 T
-mapFilterAcc( Map map, Filter filter, Acc acc, T init, T head, Ts... tail )
+mapFilterAcc( Map map, Filter filter, Acc acc, T&& init, T&& head, Ts&&... tail )
 {
     // check parameter pack length:
     // https://stackoverflow.com/questions/2770474/how-to-find-the-length-of-a-parameter-pack
     // also inspired by the example on this page:
     // https://en.cppreference.com/w/cpp/language/parameter_pack
-    T x = map( head );
+    T x = map( std::forward< T >( head ) );
     if constexpr ( sizeof...( tail ) == 0 )
     {
-        return filter( x ) ? acc( init, x ) : init;
+        return filter( x ) ? acc( std::forward< T >( init ), x )
+                           : std::forward< T >( init );
     }
     else
     {
-        return filter( x ) ? acc( x, mapFilterAcc( map, filter, acc, init, tail... ) )
-                           : mapFilterAcc( map, filter, acc, init, tail... );
+        return filter( x ) ? acc( x,
+                                  mapFilterAcc( map,
+                                                filter,
+                                                acc,
+                                                std::forward< T >( init ),
+                                                std::forward< Ts >( tail )... ) )
+                           : mapFilterAcc( map,
+                                           filter,
+                                           acc,
+                                           std::forward< T >( init ),
+                                           std::forward< Ts >( tail )... );
     }
 }
 
@@ -38,4 +50,24 @@ TEST_CASE( "map filter accumulate variadic recursion" )
     // the result of map()                               2  3  4  5
     // the result of filter()                               ^  ^  ^
     CHECK_EQ( 12, o );
+}
+
+struct Item
+{
+    int value{ 0 };
+
+    explicit Item( int x )
+      : value{ x }
+    {
+    }
+};
+
+TEST_CASE( "perfect forwarding no copying" )
+{
+    auto map = []( const Item& x ) { return Item{ x.value + 1 }; };
+    auto filter = []( const Item& x ) { return x.value > 2; };
+    auto acc = []( const Item& a, const Item& b ) { return Item{ a.value + b.value }; };
+    auto o = mapFilterAcc(
+      map, filter, acc, Item{ 0 }, Item{ 1 }, Item{ 2 }, Item{ 3 }, Item{ 4 } );
+    CHECK_EQ( o.value, 12 );
 }
