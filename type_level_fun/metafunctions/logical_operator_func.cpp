88e632f4a2fe7316659fc63862d6b821f5fd5de9
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-12-17 11:01:04 +1100

    C++17: review and clean up - restructure type_level_fun

---
+++ type_level_fun/metafunctions/logical_operator_func.cpp
@@ -0,0 +1,32 @@
+//
+// Created by weining on 17/12/20.
+//
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest/doctest.h"
+
+#include <type_traits>
+#include <sstream>
+
+// C++ 17 in detail {/60
+// some new handy template metafunctions for the logical operation
+// conjunction_v ... AND
+// disjunction_v ... OR
+// negation !
+
+template< typename OStream, typename... Ts >
+std::enable_if_t< std::conjunction_v< std::is_same< int, Ts >... > >
+printInts( OStream& os, Ts... args )
+{
+    ( os << ... << args );
+}
+
+TEST_CASE( "print integers only" )
+{
+    std::ostringstream oss;
+    printInts( oss, 1, 2, 3, 4 );
+    CHECK_EQ( oss.str(), "1234" );
+
+    // can't compile
+    //  printInts("asd", 1, 'a');
+}
